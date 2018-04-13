8f04e8c2479965000b89558f04086f5c3a130343
Marcus Behel <marcus.behel@mindspring.com>
2018-04-13 18:46:23 -0400

    Added some fixes with motion profiling and converted to idea project.

--- src/main/java/org/usfirst/frc/team2655/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team2655/robot/Robot.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.cscore.AxisCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DigitalInput;
